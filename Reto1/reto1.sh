19127e2b1e0549d2805de8f7ae9e85494a8a2850
danit3x <dvargas087@gmail.com>
2022-08-08 23:42:03 -0300

    Reto1 script

--- Reto1/reto1.sh
+++ Reto1/reto1.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+read -r -p "Continuar para crear contenedor"
 docker run -d --name servidor_web -p 8181:80 nginx
-read -p "Continuar para listar"
+echo ""
+read -r -p "Continuar para listar contenedores activos"
+docker ps
+echo ""
+read -r -p "Continuar para ver imagenes"
 docker images
-read -p "Continuar para ver imagenes"
+echo ""
+read -r -p "Continuar para detener el servidor"
+docker stop servidor_web
+echo ""
+read -r -p "Continuar para chequear que se detuvo contenedor"
+docker ps
+echo ""
+read -r -p "Continuar para remover servidor"
+docker rm servidor_web
+echo ""
+read -r -p "Continuar para revisar que se eliminó contenedor"
+docker ps -a
